5b83d6e621bbd71192208b57c70d04e92a6cf44b
Hong <13su@naver.com>
2018-11-09 18:53:01 +0900

    콘솔

--- sawtooth-explorer-master/src/app/pipes/base64-decode/base64-decode.pipe.ts
+++ sawtooth-explorer-master/src/app/pipes/base64-decode/base64-decode.pipe.ts
@@ -45,10 +45,11 @@ export class Base64DecodePipe implements PipeTransform {
     // attempt to decode from base64 if needed
     if (base64Regex.test(value)) { // 패턴이 있는지를 봄. 즉 base64로 인코딩된 문자인지를 탐지하는 것. 
       try {
-        decodedValue = window.atob(value); // atob : base64 디코딩 메소드 
+        decodedValue = atob(value); // atob : base64 디코딩 메소드 
       } catch(e) {
       }
     }
+    console.log(decodedValue); // 테스트용
 
     return decodedValue; // 여기서 리턴하는데, 
   }
