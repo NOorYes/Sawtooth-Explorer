bdf9fb3d91ee3c57f9641d6319eb0606deae919d
Hong <13su@naver.com>
2018-11-23 21:45:01 +0900

    투스트링까진 정상

--- sawtooth-explorer-master/src/app/transactions/transaction/transaction.component.ts
+++ sawtooth-explorer-master/src/app/transactions/transaction/transaction.component.ts
@@ -127,7 +127,7 @@ export class TransactionComponent implements OnInit, OnChanges {
     this.parsearray[2] = array[1].match(/[A-Z]{2}-[0-9]{3}-[0-9]{2}-[A-Z]{2}/g); // 공급회사 코드
     this.parsearray[3] = array[1].match(/[가-힣]+J/g); // 공급회사명
     console.log(this.parsearray[3].toString());
-    this.parsearray[3] =  this.parsearray[3].toString().substring(0, this.parsearray[3].length-1); 
+    this.parsearray[3] =  this.parsearray[3].toString().slice(0, -1); 
     console.log(this.parsearray[3]);
     this.parsearray[4] = array[1].match(/\d{3}-\d{2}-\d{5}/g); // 사업자 등록번호
     this.parsearray[5] = array[1].match(/[가-힣]+Z/g); // 대표자명
