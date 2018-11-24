d9cb86b18cd734b74f44a2bc532966ad9cd8e24f
Hong <13su@naver.com>
2018-11-25 00:12:35 +0900

    오류수정

--- sawtooth-explorer-master/src/app/transactions/transaction/transaction.component.ts
+++ sawtooth-explorer-master/src/app/transactions/transaction/transaction.component.ts
@@ -132,7 +132,8 @@ export class TransactionComponent implements OnInit, OnChanges {
     this.parsearray[4] = array[1].match(/\d{3}-\d{2}-\d{5}/g); // 사업자 등록번호
     this.parsearray[5] = array[1].match(/[가-힣]+Z/g); // 대표자명
     this.parsearray[5] =  this.parsearray[5].toString().slice(0, -1);
-    this.parsearray[6] = array[1].match(/\d{3}-\d{3,4}-\d{4}/g); // 공급회사 전화번호
+    this.parsearray[6] = array[1].match(/\d{3}-\d{3,4}-\d{4}b/g); // 공급회사 전화번호
+    this.parsearray[6] =  this.parsearray[6].toString().slice(0, -1);
     this.parsearray[7] = array[1].match(/\d{3}-\d{3,4}-\d{4}j/g); // 공급회사 팩스번호
     this.parsearray[7] =  this.parsearray[7].toString().slice(0, -1);
     this.parsearray[8] = array[1].match(/((([가-힣]+(시|도)|[서울]|[인천]|[대구]|[광주]|[부산]|[울산])( |)[가-힣]+(시|군|구)( |))[가-힣]+([가-힣]|(\d{1,5}(~|-)\d{1,5})|\d{1,5})+(로|길)( |)(\d)+)Â/g); // 공급회사 주소
@@ -151,9 +152,9 @@ export class TransactionComponent implements OnInit, OnChanges {
     this.parsearray[13] = this.parsearray[13].toString().slice(0, -1); 
     this.parsearray[14] = array[1].match(/((([가-힣]+(시|도)|[서울]|[인천]|[대구]|[광주]|[부산]|[울산])( |)[가-힣]+(시|군|구)( |))[가-힣]+([가-힣]|(\d{1,5}(~|-)\d{1,5})|\d{1,5})+(로|길)( |)(\d)+)â/g); // 작성자 회사의 주소
     //this.parsearray[11] = this.parsearray[11].toString().slice(0, -1); // 값이 없어서 오류남
-    this.parsearray[15] = array[1].match(/[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*.[a-zA-Z]{2,3}/g); // 작성자 이메일
+    this.parsearray[15] = array[1].match(/[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*.[a-zA-Z]{2,3}ê/g); // 작성자 이메일
     this.parsearray[15] = this.parsearray[15].toString().replace("u0011", "");
-
+    this.parsearray[15] =  this.parsearray[15].toString().slice(0, -1);
     this.parsearray[16] = array[2].match(/(19|20)\d{2}-(0[1-9]|1[012])-(0[1-9]|[12][0-9]|3[0-1])ú/g); // 작성날짜 
     this.parsearray[16] = this.parsearray[16].toString().slice(0, -1); 
     this.parsearray[17] = array[2].match(/kFop/g); // 작성시스템
